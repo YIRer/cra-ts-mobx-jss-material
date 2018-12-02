d583105e3d9162d520e111b9ef3fd5819b4cd8cf
Yirer <xeroonelove@gmail.com>
2018-12-02 18:47:50 +0900

    initial setting CRA v2, Mobx, Typescript, JSS, Material-UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,32 @@
 import React, { Component } from 'react';
+import { withStyles } from '@material-ui/core';
+import Button from '@material-ui/core/Button';
+import { observable } from "mobx";
+import { observer } from "mobx-react"
 import logo from './logo.svg';
+import styles from './styles';
 import './App.css';
 
-class App extends Component {
+@observer
+class App extends Component<{ classes: { [index: string]: string } }> {
+  @observable
+  body: string = "Csadsdoolsakdsadjk!"
   render() {
+    const { classes: cls } = this.props;
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.tsx</code> and save to reload.
+            {this.body}
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <Button className={cls.paragraph}>
+            Learn Reactsdksoadjsksakdsajdsad
+          </Button>
         </header>
       </div>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
